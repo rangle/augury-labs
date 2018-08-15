68185d67ce068edcc3e31e4676b26c1af4f029db
santiago-elustondo <elus2012@gmail.com>
2018-08-15 16:09:11 -0400

    HealthIndicatorOverlay plugin

---
+++ plugins/ovelay-health-indicator/src/triggers/too-many-cycles.ts
@@ -0,0 +1,16 @@
+
+const TIME_WINDOW = 2500
+const MAX_NUM_CYCLES_IN_WINDOW_THRESHOLD = 10
+
+export function tooManyCycles(cyclesOverTime: Map<number, any /* cycle */>) {
+
+  const timestamps = Array.from(cyclesOverTime.keys())
+  const latest = timestamps.slice(-1)[0]
+  const timestampsInWindow = timestamps.filter(t => latest - t < TIME_WINDOW)
+
+  if (timestampsInWindow.length > MAX_NUM_CYCLES_IN_WINDOW_THRESHOLD) {
+    return true
+  } else
+    return false
+
+}
