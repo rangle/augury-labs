87e89e57e8f38070a6ef5533521279601d3642c5
Santiago Elustondo <elus2012@gmail.com>
2018-08-28 15:21:23 -0400

    update reducers to return { result } add some probes

--- packages/core/src/probes/registry.ts
+++ packages/core/src/probes/registry.ts
@@ -1,12 +1,16 @@
 import { ProbeRegistry } from '../framework/probes/probe-registry'
 
-import { NgZoneProbe } from './ng-zone-probe'
-import { NgDebugProbe } from './ng-app-debug-probe'
+import { RootZoneProbe } from './root-zone'
+import { NgZoneProbe } from './ng-zone'
+import { NgDebugProbe } from './ng-debug'
 import { ComponentHooksProbe } from './component-hooks'
+import { ModuleMethodsProbe } from './module-methods'
 
 export const probeRegistry: ProbeRegistry
   = [
+    RootZoneProbe,
     NgZoneProbe,
     NgDebugProbe,
-    ComponentHooksProbe
+    ComponentHooksProbe,
+    ModuleMethodsProbe
   ]
