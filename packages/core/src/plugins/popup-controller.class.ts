9addafa30ad0b1be7a0d10967c342de236be4e7b
feeloor <felix@khoi.se>
2019-04-24 14:49:05 -0400

    refactor(frontend): persisting position and size of popup

--- packages/core/src/plugins/popup-controller.class.ts
+++ packages/core/src/plugins/popup-controller.class.ts
@@ -1,11 +1,22 @@
 import { AuguryWindow } from '../augury-window.interface';
 
+interface WindowSizeAndPosition {
+  position: { x: number; y: number };
+  size: { width: number; height: number };
+}
+
 export class PopupController {
   public readonly window: AuguryWindow;
+  protected storageKey: string;
   private readonly onUnload: () => void;
 
   constructor(protected readonly name: string) {
     this.onUnload = this.kill.bind(this);
+    this.storageKey = `${this.name
+      .toLowerCase()
+      .split(' ')
+      .join('-')}-size-position`;
+
     this.window = this.initializeWindow();
   }
 
@@ -29,16 +40,48 @@ export class PopupController {
       throw new Error('Please allow popup windows');
     }
 
-    pluginWindow.moveTo(0, 0);
-    pluginWindow.resizeTo(screen.width, screen.height);
+    const sizeAndPosition = this.getWindowSizeAndPositionFromStorage();
+    pluginWindow.moveTo(sizeAndPosition.position.x, sizeAndPosition.position.y);
+    pluginWindow.resizeTo(sizeAndPosition.size.width, sizeAndPosition.size.height);
 
     window.addEventListener('unload', this.onUnload);
 
     return pluginWindow;
   }
 
+  private getWindowSizeAndPositionFromStorage(): WindowSizeAndPosition {
+    const defaultSizeAndPosition = {
+      position: { x: 0, y: 0 },
+      size: { width: screen.width, height: screen.height },
+    };
+    const fromStorage = localStorage.getItem(this.storageKey);
+
+    return !!fromStorage ? JSON.parse(fromStorage) : defaultSizeAndPosition;
+  }
+
+  private storeWindowSizeAndPosition() {
+    // Fallback to screen width/height if popup is already closed
+    const outerWidth = this.window.outerWidth === 0 ? screen.width : this.window.outerWidth;
+    const outerHeight = this.window.outerHeight === 0 ? screen.height : this.window.outerHeight;
+
+    const windowSizeAndPosition: WindowSizeAndPosition = {
+      position: {
+        x: this.window.screenX,
+        y: this.window.screenY,
+      },
+      size: {
+        width: outerWidth,
+        height: outerHeight,
+      },
+    };
+
+    localStorage.setItem(this.storageKey, JSON.stringify(windowSizeAndPosition));
+  }
+
   private kill() {
+    this.storeWindowSizeAndPosition();
     this.window.close();
+
     window.removeEventListener('unload', this.onUnload);
   }
 }
