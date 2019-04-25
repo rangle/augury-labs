ce7fbe6aac62d57b9a09c5437717a3b0c38abe68
feeloor <felix@khoi.se>
2019-04-25 09:34:48 -0400

    refactor(frontend): addressing comments related to template expressions

--- packages/frontend/src/app/components/header-menu/header-menu.component.ts
+++ packages/frontend/src/app/components/header-menu/header-menu.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+interface MenuTab {
+  id: number;
+  tabName: string;
+  active?: boolean;
+}
 
 @Component({
   selector: 'ag-header-menu',
   templateUrl: './header-menu.component.html',
   styleUrls: ['./header-menu.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderMenuComponent {
-  public tabs = ['Performance Profiler'];
-  public activeTabId = 0;
+  public tabs: MenuTab[] = [{ id: 0, tabName: 'Performance Profiler', active: true }];
+
+  public trackByTabId(index, tab: MenuTab) {
+    return tab ? tab.id : null;
+  }
+
+  public onSetActiveTab(id: number) {
+    this.changeTabActiveStatus(this.tabs.find(tab => tab.active), false);
+    this.changeTabActiveStatus(this.tabs.find(tab => tab.id === id), true);
+  }
+
+  private changeTabActiveStatus(tab: MenuTab, active: boolean) {
+    if (tab) {
+      tab.active = active;
+    }
+  }
 }
